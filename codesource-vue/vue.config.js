319af66933fd788277f7a74d368a18dfd58623fe
吾茗l_l <50832635+wuming082@users.noreply.github.com>
2024-12-25 22:03:02 +0800

    Update vue.config.js

--- codesource-vue/vue.config.js
+++ codesource-vue/vue.config.js
@@ -3,6 +3,23 @@ module.exports = defineConfig({
   transpileDependencies: true
 });
 
-module.exports = {
-  publicPath: './',
+// module.exports = {
+//   publicPath: './',
+// }
+
+let publicPath = "";
+const port = process.env.VUE_APP_PORT || 8080;
+if(port == "8080"){
+  let publicPath = './';
+}else{
+  let publicPath = `http://baoding.dreamsky0822.asia:${port}/`;
 }
+
+
+module.exports = {
+  devServer: {
+    port: port, // 使用变量配置端口
+    allowedHosts: 'all',
+  },
+  publicPath: publicPath, // 使用组合后的公共路径
+};
