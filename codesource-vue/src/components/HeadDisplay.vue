e87ea273f7501296bfce995cca9283c39658fb9c
XZ <2326268900@qq.com>
2024-12-31 18:20:15 +0000

    停用了原先的BottonList，更改为PageMenu+UserAvatar

--- codesource-vue/src/components/HeadDisplay.vue
+++ codesource-vue/src/components/HeadDisplay.vue
@@ -1,9 +1,10 @@
 <template>
     <div id="Head">
-        <button id="projectName">
+        <show-time></show-time>
+        <el-button id="projectName">
             project name
-        </button>
-        <button id="pieChartButton">
+        </el-button>
+        <el-button id="pieChartButton">
             <el-progress
                 type="circle"
                 :percentage="inlong"
@@ -11,18 +12,22 @@
                 :width="50"
                 :color="customColors"
             />
-        </button>
-        <Botton-list id="bottonlist"></Botton-list>
-        <show-time></show-time>
+        </el-button>
+        <!-- <Botton-list id="bottonlist"></Botton-list>，注释掉了原先的按钮列表 -->
+        <UserAvatar id="useravatar"></UserAvatar>
+        <PageMenu id="pagemenu"></PageMenu>
         <!-- 临时隐藏了原有的进度条 -->
         <progress-bar-main v-if="false" id="progress" :stroke-width="80" :Linelong="inlong"></progress-bar-main>
     </div>
 </template>
 
 <script>
-import BottonList from './HeaddisplayComponents/BottonList.vue';
+// import BottonList from './HeaddisplayComponents/BottonList.vue';
 import ShowTime from './HeaddisplayComponents/ShowTime.vue';
-import ProgressBarMain from './ProgressBarMain.vue';
+// import ProgressBarMain from './ProgressBarMain.vue';
+import PageMenu from './HeaddisplayComponents/PageMenu.vue';
+import UserAvatar from './HeaddisplayComponents/UserAvatar.vue';
+
 
 export default{
     data(){
@@ -38,10 +43,12 @@ export default{
         }
     },
     components:{
-        BottonList,
-        ShowTime,
-        ProgressBarMain,
-    },
+    // BottonList,
+    ShowTime,
+    // ProgressBarMain,
+    PageMenu,
+    UserAvatar,
+},
     props:{
         inlong:Number
     }
@@ -69,6 +76,8 @@ export default{
     -webkit-user-select: none; /* 适用于谷歌浏览器和Safari */ -moz-user-select: none; /* 适用于火狐浏览器 */ -ms-user-select: none; /* 适用于Internet Explorer/Edge */ user-select: none; /* 适用于支持CSS3的浏览器 */
     position: absolute;
     top: 50%; 
+    padding: 0%;
+    margin: 0%;
     transform: translateY(-50%); 
 }
 
@@ -80,11 +89,12 @@ export default{
     user-select: none; /* 适用于支持CSS3的浏览器 */
     position: fixed; 
     top: 0px;
-    left: calc(65%); /* 紧挨着 projectName 右侧 */
+    left: calc(64.2%); /* 紧挨着 projectName 右侧 */
     text-align: center; 
-    height: 65px;
+    height: 62px;
+    width: 62px;
+    margin-bottom: 3px;
     z-index: 1000; 
-
     /* 按钮样式 */
     color: #484848; 
 
@@ -95,12 +105,12 @@ export default{
     border: none; /* 移除边框 */
     box-shadow: none; /* 移除阴影 */
     outline: none; /* 移除点击时的轮廓 */
-    transition: transform 0.2s ease, box-shadow 0.2s ease;
+    transition: transform 0.1s ease, box-shadow 0.1s ease;
 }
 
 #pieChartButton:hover {
     transform: translateY(2px); /* 悬浮时略微下凹 */
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); /* 悬浮时添加阴影 */
+    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.1); /* 悬浮时添加阴影 */
 }
 
 #pieChartButton:active {
@@ -118,7 +128,8 @@ export default{
     transform: translateX(-50%); /* 通过偏移实现精确居中 */
     text-align: center; /* 文本居中 */
     width: 30%; /* 宽度根据内容自适应 */
-    height: 65px;
+    height: 62px;
+    margin-bottom: 3px;
     z-index: 1000; /* 确保元素在最上层 */
 
     /* 按钮样式 */
@@ -134,18 +145,34 @@ export default{
     border: none; /* 移除边框 */
     box-shadow: none; /* 移除阴影 */
     outline: none; /* 移除点击时的轮廓 */
-    transition: transform 0.2s ease, box-shadow 0.2s ease; /*缓动效果*/
+    transition: transform 0.1s ease, box-shadow 0.1s ease; /*缓动效果*/
 }
 
 #projectName:hover {
     transform: translateX(-50%) translateY(2px); /* 悬浮时略微下凹 */
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); /* 悬浮时添加阴影 */
+    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.1); /* 悬浮时添加阴影 */
 }
 
 #projectName:active {
     transform: translateX(-50%) translateY(4px); /* 点击时进一步下凹 */
     color: #303030; /* 点击时颜色加深 */
     box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1); /* 点击时阴影变小 */
+}
+
+#pagemenu{
+    -webkit-user-select: none; /* 适用于谷歌浏览器和Safari */ -moz-user-select: none; /* 适用于火狐浏览器 */ -ms-user-select: none; /* 适用于Internet Explorer/Edge */ user-select: none; /* 适用于支持CSS3的浏览器 */
+    position: fixed; /* 固定在页面上 */
+    top: 0px; /* 距离顶部距离 */
+    right: 0px; /* 距离右侧距离 */
+    z-index: 1000; /* 确保元素在最上层 */
+}
+
+#useravatar{
+    -webkit-user-select: none; /* 适用于谷歌浏览器和Safari */ -moz-user-select: none; /* 适用于火狐浏览器 */ -ms-user-select: none; /* 适用于Internet Explorer/Edge */ user-select: none; /* 适用于支持CSS3的浏览器 */
+    position: fixed; /* 固定在页面上 */
+    top: 0px; /* 距离顶部距离 */
+    right: 80px; /* 距离右侧距离 */
+    z-index: 1000; /* 确保元素在最上层 */
 }
 
 </style>
---
+++ codesource-vue/src/components/HeaddisplayComponents/UserAvatar.vue
@@ -0,0 +1,44 @@
+<template>
+    <el-button class="user-button" circle>
+        <span class="icon">X</span>
+    </el-button>
+</template>
+
+<script>
+export default {
+    data() {
+        return {};
+    },
+    methods: {}
+};
+</script>
+
+<style scoped>
+.user-button {
+    width: 55px; /* 设置按钮宽度 */
+    height: 55px; /* 设置按钮高度 */
+    margin: 5px;
+    padding: 0; /* 去除内边距 */
+    background-color: #e9f5ff; /* 背景透明 */
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer; /* 鼠标悬停时显示为手型 */
+    transition: transform 0.1s ease, box-shadow 0.1s ease; /*缓动效果*/
+}
+
+.user-button:hover {
+    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.1); /* 悬浮时添加阴影 */
+}
+
+.user-button:active {
+    transform: translateY(2px); /* 悬浮时略微下凹 */
+    color: #303030; /* 点击时颜色加深 */
+    box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1); /* 点击时阴影变小 */
+}
+
+.icon {
+    font-size: 35px; /* 设置图标大小 */
+    color: #606060; /* 设置图标颜色 */
+}
+</style>
